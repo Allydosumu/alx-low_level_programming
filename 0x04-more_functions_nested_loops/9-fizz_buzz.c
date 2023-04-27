47018562cf873395e9ff349d4aebdea7069986d4
allydosumu <alimatdosumu.ad@gmail.com>
2023-04-27 21:46:32 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - This is a fizz buzz program
@@ -10,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
