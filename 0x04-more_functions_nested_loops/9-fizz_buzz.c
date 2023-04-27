b377eeadd67fc2d88d8624bab5e66669f92ed2e5
allydosumu <alimatdosumu.ad@gmail.com>
2023-04-27 21:51:57 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,8 +4,10 @@
 /**
  * main - This is a fizz buzz program
  *
- * Return: 0
+ * Description: Prints Fizz for multiples of 3,
+ * Buzz for multiples of 5 and FizzBuzz for multiples of 15
  *
+ * Return: 0
  */
 int main(void)
 {
