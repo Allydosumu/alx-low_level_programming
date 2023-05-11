da60c2adc66fbb266f92099d155b80896c8b87bd
allydosumu <alimatdosumu.ad@gmail.com>
2023-05-11 19:25:23 +0100

    5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * find_root_number - This program writes a function that
+ * finds the square root of a number.
+ * @root_number: the test number
+ * @n: input
+ *
+ * Return: natural square root
+ */
+int find_root_number(int n, int root_number)
+{
+	if (root_number * root_number > n)
+	{
+		return (-1);
+	}
+	else if (root_number * root_number == n)
+	{
+		return (root_number);
+	}
+	return (find_root_number(n, root_number + 1));
+}
+
+/**
+ * _sqrt_recursion - This program writes the function that
+ * returns the natural square root of a number
+ * @n: input
+ * @root_number: test number
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int root_number = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (find_root_number(n, root_number));
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(int n);
+int find_root_number(int n, int root_number);
