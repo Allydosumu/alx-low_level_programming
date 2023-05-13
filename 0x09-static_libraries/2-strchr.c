72a6fa465b67cd2b5ba5616e88fd43ade4332ae5
allydosumu <alimatdosumu.ad@gmail.com>
2023-05-13 21:23:13 +0100

    Static libraries

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,21 +1,22 @@
 #include "main.h"
+#define NULL 0
 /**
  * *_strchr - This program writes a function that
  * locates a character in a string.
  * @s: string
  * @c: character
- * Return: first occurrence of character c.
+ * Return: first occurrence of character c and NULL
+ * if not found..
  */
 char *_strchr(char *s, char c)
 {
-	int a;
+	int i = 0;
 
-	for (a = 0; *(s + a) != '\0'; a++)
-	{
-		if (*(s + a) == c)
-		{
-			return (s + a);
-		}
-	}
-	return (0);
+	while (s[i] != '\0' && s[i] != c) /* find match */
+		i++;
+
+	if (s[i] == c) /* if match, assign to address */
+		return (&s[i]);
+	else
+		return (NULL);
 }
