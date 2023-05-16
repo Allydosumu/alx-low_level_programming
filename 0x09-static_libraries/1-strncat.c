a98d1242bf476d81ae0bf20bc958500deab1b803
allydosumu <alimatdosumu.ad@gmail.com>
2023-05-16 16:15:38 +0100

    Static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - This program writes a function that
+ * concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ * @n: amount of bytes from src.
+ * Return: 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - This program writes a function that
+ * computes the absolute value of an integer
+ *
+ * @d: d is an integer
+ *
+ * Return: 0
+ *
+ */
+int _abs(int d)
+{
+	if (d > 0)
+	{
+		return (d);
+	}
+	else if (d < 0)
+	{
+		return (-d);
+	}
+	else
+	{
+		return (d);
+	}
+}
