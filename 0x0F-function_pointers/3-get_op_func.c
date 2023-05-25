0ce814c21cd2ec0cf16900e5189caa9617cecbcd
allydosumu <alimatdosumu.ad@gmail.com>
2023-05-26 00:17:58 +0100

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,11 +9,11 @@
  * Return: A pointer to the function corresponding to
  * the operator
  */
-int (*get_op_func(char *s))(int,int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-	 	{"-", op_sub},
+		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
